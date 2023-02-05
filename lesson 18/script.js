763294012e5b2dfae0e4a37ff19fb41ad0594526
MichaelCoolBlood <TsibinMishaPas2010>
2023-02-05 19:55:25 +0700

    tr

--- lesson 18/script.js
+++ lesson 18/script.js
@@ -1,2 +1,8 @@
-let color = prompt ("what");
-alert(`qwerty - ${color}`)
+"use strict"
+let hasFinishedEducation = false; 
+const passFinalTest = true;
+ //Если условие верно то изменить переменную hasFinishedEducation на true
+if(passFinalTest) hasFinishedEducation=true; //ошибка в переменной hasFinishedEducation
+ 
+//Если условие верно то вывести текст
+if(hasFinishedEducation) console.log("Поздравляем! Ты закончил обучение!");
