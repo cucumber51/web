9682d22646c7d730fe78862d1228fc298d20b428
alima-lol <alima18_04@mail.ru>
2023-02-12 19:53:53 +0700

    ax

---
+++ lesson 19/script.js
@@ -0,0 +1,35 @@
+//function juicemaker(apples, oranges=5)
+////if (oranges === undefined){
+////    oranges = 'без испольования'
+////}
+//{console.log(apples, oranges);
+//const juice = `ваш сок из ${apples} яблок и ${oranges}`
+//return juice;}
+//let x = juicemaker(2,);
+//console.log(juicemaker(2))
+
+//function juicemaker(apples, oranges){
+////if (oranges === undefined){
+////    oranges = 'без испольования'
+////}
+//oranges = oranges || 'без использования';
+//console.log(apples, oranges);
+//const juice = `ваш сок из ${apples} яблок и ${oranges}`
+//return juice;}
+
+//console.log(juicemaker(6))
+
+//retutn без значения это выход из функции
+//let a = prompt('1');
+//let b = prompt('2');
+//function min(a,b){
+//if (a>b) return b;
+//return a;
+//} 
+//alert(min(a,b))
+
+const students = ["лиза", "тимур","ясинбек"]
+console.log(students.at(-1))
+//добавить элемент
+students[10] = "Даша"
+console.log(students[7])
