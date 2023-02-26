0b6e8f27c11c6619a8d0c41688ad1c449e0d7cd0
movavi_school <Nagornyak.2005@mai.ru>
2023-02-26 19:54:37 +0700

    gglugk.

---
+++ lesson 22/script.js
@@ -0,0 +1,41 @@
+// let b=[];
+// let a = [true,1,'qwe', [1,4,6],{a:1,b:2}]
+// for (q=0;q<a.length;q++){
+//     console.log(a[q])
+//     b.push(typeof(a[q]))
+// }
+// console.log(b)
+
+// let a=[];
+// let b=[1920,2013,2020,1996,1630];
+// for(ab=0; ab<b.length;ab++){
+//     a.push(2023-b[ab])};
+// console.log(a);
+
+// let b = []
+// let a=[1920,2013,2020,1996,1630];
+// for(ab=0; ab<a.length;ab++){
+//     b.unshift(a[ab])
+    
+// }
+// console.log(b)
+
+// for(jump=1; jump<=10;jump++){
+//     if (jump==3){continue}
+//         console.log("прыг "+jump)
+
+//     for (sit=1;sit<=3;sit++){
+//         console.log("присяд"+sit)
+//     }
+// }
+
+// let x=0;
+// while (x<10){x++
+// console.log("prig "+x)};
+
+// while(true){
+//     a = prompt('')
+//     if (a>10){
+//         continue}
+// }
+100слайд
