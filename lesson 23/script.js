d1e0bf412d38c57df06e33a9019738c44da7f087
lexasd <atomilov720@gmail.com>
2023-03-05 19:57:19 +0700

    xxcV sd

---
+++ lesson 23/script.js
@@ -0,0 +1,23 @@
+// console.log(Math.ceil(5.001)); //вверх
+// console.log(Math.floor(5.9999)); //вниз
+// console.log(Math.round(5.6)); // округляет математически
+// console.log(Math.trunc(5.999)); //убирает дробную часть
+// console.log(5.354.toFixed(2)); //округляет до двух знаков после запятой
+//alert(1.35)
+
+
+// let a = Math.random()*6;
+// console.log(a);
+
+// let red = document.querySelector('.red')
+// red.innerHTML ='Приффки'
+
+// let bb = document.querySelector("#blue")
+// bb.innerHTML = 'Пока'
+
+// let inp = document.querySelector(".chiki-piki")
+// inp.value = 'Moscow'
+
+function myFunction( ) {
+    document.querySelector(".myText").value = "Johnny";
+}
